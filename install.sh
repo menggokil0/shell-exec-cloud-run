96af456373ebd7ffe4085fdace7c9c8723224af4
calebsto <82493755+calebsto@users.noreply.github.com>
2021-04-26 16:41:35 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,3 @@ gcloud services enable run.googleapis.com
 git clone https://github.com/calebsto/shell-exec-cloud-run.git
 cd shell-exec-cloud-run/python
 make build && make deploy
-gcloud run services describe cloud-run-exec-python --format='value(status.url)' --region us-central1 --platform managed
-gcloud auth print-identity-token
