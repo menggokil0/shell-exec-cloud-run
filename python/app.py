d755433af19880b61a6e96bc7cb20408abba9682
calebsto <82493755+calebsto@users.noreply.github.com>
2021-05-30 18:28:53 +0700

    Update app.py

--- python/app.py
+++ python/app.py
@@ -5,10 +5,19 @@ from flask import Flask, request, make_response
 print("Started...")
 app = Flask(__name__)
 
-@app.route('/')
-def func1():
-    print("Running")
-    return "Hello World"
+@app.route('/', methods=['GET', 'POST'])
+def route:
+    command = request.data.decode('utf-8')
+    try:
+        completedProcess = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, timeout=60*60, universal_newlines=True)
+        response = make_response(completedProcess.stdout, 200)
+        response.mimetype = "text/plain"
+        return response
+    except subprocess.TimeoutExpired:
+        response = make_response("Timedout", 400)
+        response.mimetype = "text/plain"
+        return response
+    return "/"
 
 @app.route('/exe', methods=['POST'])
 def route_exec():
