1d066aa48e69dfcf8fddb872f6d05e08f9b134d2
calebsto <82493755+calebsto@users.noreply.github.com>
2021-06-02 22:13:02 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 gcloud services enable run.googleapis.com
 git clone https://github.com/calebsto/shell-exec-cloud-run.git
-cd shell-exec-cloud-run/nodejs
+cd shell-exec-cloud-run/python
 make build && make deploy
