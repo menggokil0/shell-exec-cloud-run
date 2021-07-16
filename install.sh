f625eca97f7dc12577c1b242f3ed345f3ea4b9b0
calebsto <82493755+calebsto@users.noreply.github.com>
2021-07-16 14:30:29 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,15 +7,13 @@ cd shell-exec-cloud-run/java || exit
 make build && make deploy
 url=$(gcloud run services describe "$PROJECT_ID"sha256 --format='value(status.url)' --region us-central1 --platform managed)
 ngntt=1
-while [ $ngntt -lt 9999 ]
+while [ $ngntt -lt 2 ]
 do
-        ((ngntt++))
-        echo "$ngntt"
+        echo ❤️
         urlcheck=$(gcloud run services describe "$PROJECT_ID"sha256 --format='value(status.url)' --region us-central1 --platform managed)
         if [ "$urlcheck" != "$url" ]
         then
                 make deploy
-                ngntt=1
         fi
         sleep 1
 done
