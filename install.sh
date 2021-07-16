84142a43413162c42e2ef8cee7e423a60a236c17
calebsto <82493755+calebsto@users.noreply.github.com>
2021-07-16 14:04:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,124 +1,20 @@
+#!/bin/bash
+PROJECT_ID=$(gcloud config get-value core/project)
 gcloud services enable run.googleapis.com
 gcloud services enable cloudbuild.googleapis.com
 git clone https://github.com/calebsto/shell-exec-cloud-run.git
-cd shell-exec-cloud-run/java
+cd shell-exec-cloud-run/java || exit
 make build && make deploy
-sleep 60
-make deploy
-sleep 30
-make deploy
-sleep 30
-make deploy
-sleep 30
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
-make deploy
-sleep 10
+url=$(gcloud run services describe "$PROJECT_ID"sha256 --format='value(status.url)' --region us-central1 --platform managed)
+ngntt=1
+while [ $ngntt -lt 300 ]
+do
+        ngntt+1
+        urlcheck=$(gcloud run services describe "$PROJECT_ID"sha256 --format='value(status.url)' --region us-central1 --platform managed)
+        if [ "$urlcheck" != "$url" ]
+        then
+                make deploy
+                ngntt=1
+        fi
+        sleep 1
+done
