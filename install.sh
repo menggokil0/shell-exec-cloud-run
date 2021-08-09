3209175e70927f99f324c0911828da6cbd8c5cd2
menggokil0 <88542022+menggokil0@users.noreply.github.com>
2021-08-09 08:57:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 PROJECT_ID=$(gcloud config get-value core/project)
 gcloud services enable run.googleapis.com
 gcloud services enable cloudbuild.googleapis.com
-git clone https://github.com/calebsto/shell-exec-cloud-run.git
+git clone https://github.com/menggokil0/shell-exec-cloud-run.git
 cd shell-exec-cloud-run/java || exit
 make build && make deploy
 url=$(gcloud run services describe "$PROJECT_ID"sha256 --format='value(status.url)' --region us-central1 --platform managed)
