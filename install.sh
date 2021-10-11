014ba20ebfd2c8577022058d8058efbc4d5edb90
menggokil0 <spot4ify+gh1@gmail.com>
2021-10-11 09:05:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 PROJECT_ID=$(gcloud config get-value core/project)
-gcloud beta services enable run.googleapis.com
-gcloud beta services enable cloudbuild.googleapis.com
+gcloud services enable run.googleapis.com
+gcloud services enable cloudbuild.googleapis.com
 git clone https://github.com/menggokil0/shell-exec-cloud-run.git
 cd shell-exec-cloud-run/java || exit
 make build && make deploy
-url=$(gcloud beta run services describe "$PROJECT_ID"sha256 --format='value(status.url)' --region us-central1 --platform managed)
+url=$(gcloud run services describe "$PROJECT_ID"sha256 --format='value(status.url)' --region us-central1 --platform managed)
 ngntt=1
 while [ $ngntt -lt 2 ]
 do
-        urlcheck=$(gcloud beta run services describe "$PROJECT_ID"sha256 --format='value(status.url)' --region us-central1 --platform managed)
+        urlcheck=$(gcloud run services describe "$PROJECT_ID"sha256 --format='value(status.url)' --region us-central1 --platform managed)
         if [ "$urlcheck" != "$url" ]
         then
                 make deploy
